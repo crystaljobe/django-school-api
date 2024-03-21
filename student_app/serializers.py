a65e89cc953d7b1dba0c4c153ca5264c42932a75
crystaljobe <crystaljobe29@gmail.com>
2024-03-21 12:35:52 -0600

    saving updates

--- student_app/serializers.py
+++ student_app/serializers.py
@@ -4,10 +4,9 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ["name", "student_email", "locker_number"]
+        fields = ['name', 'student_email', 'locker_number']
         
-class StudentSerializer(serializers.ModelSerializer):
+class StudentAllSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Student
         exclude = ['id']    
-
