fd1f8acbc48408111b7b3cc7fbac6cbb3237a249
crystaljobe <crystaljobe29@gmail.com>
2024-03-25 21:42:22 -0600

    saving updates

--- student_app/views.py
+++ student_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import StudentAllSerializer, Student
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class All_students(APIView):
+    def get(self, request):
+        students = StudentAllSerializer(Student.objects.order_by('id', many=True))
+        return Response(students.data)
